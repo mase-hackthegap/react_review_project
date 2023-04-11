0357dd104cde94e9332219e7f788c2edc3ea8686
Mase <48336140+MaseSantos@users.noreply.github.com>
2023-04-11 18:04:38 -0500

    Demo from Monday 4/10

--- src/components/InputMessage.js
+++ src/components/InputMessage.js
@@ -1,36 +1,96 @@
-import React, {useState} from "react";
+import React, {useState} from 'react';
 
 function InputMessage(){
-  const [message, setMessage] = useState('');
-  const [submittedMessage, setSubmittedMessage] = useState('');
+
+  //This is to create the template of the objects that will later
+  //be in the messages array that hold all of the messages
+  const [messageInfo, setMessageInfo] = useState({
+    name: '',
+    text: ''
+  });
+
+  //this is the array that will hold our messages 
+  const [messages, setMessages] = useState([]);
 
   const handleInputChange = (event) => {
-    //event here = the input change as the user types
-    // the target is the input itself
-    // the value is the content being typed in the input 
-    setMessage(event.target.value);
-    console.log(event.target.value);
-  }
+    setMessageInfo({
+      ...messageInfo,
+      [event.target.name]: event.target.value
+    });
+    console.log(`This is the input change: ${event.target.name}: ${event.target.value}`);
+  };
 
-  const handleSubmit = () => {
-    setSubmittedMessage(message);
-    console.log('message submitted');
-    setMessage('');
-  }
-  
+  const handleSubmit = (event) =>{
+    event.preventDefault();
+    //finishing up the packaging of the new message for the array
+    const newMessage = {
+      name: messageInfo.name,
+      text: messageInfo.text
+    };
+    //Actually sending the new message into the array
+    setMessages([...messages, newMessage]);
+    //this clears out the inputs
+    setMessageInfo({
+      name: '',
+      text: ''
+    })
+  };
+
   return(
     <>
-    <p>You have submitted: {submittedMessage}</p>
-    <input 
-      type="text" 
-      value={message} 
-      onChange={handleInputChange}
-    />
-    <button onClick={handleSubmit}>Submit</button>
+      <h1>My Message Board</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name of Messenger:
+          <input 
+            type="text" 
+            name="name" 
+            value={messageInfo.name}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          Message to Add:
+          <input 
+            type="text" 
+            name="text" 
+            value={messageInfo.text}
+            onChange={handleInputChange}
+          />
+        </label>
+        <button type="submit">Submit Your Message</button>
+      </form>
+      {messages.map((message, index)=>(
+        <div key={index}>
+          <h3>{message.name}</h3>
+          <p>{message.text}</p>
+        </div>
+      ))}
     </>
-  )
-
+  );
 }
 
 export default InputMessage;
 
+// !!! remember all of the kinds of inputs you can use: !!!
+// <input type="button"/>
+// <input type="checkbox"/>
+// <input type="color"/>
+// <input type="date"/>
+// <input type="datetime-local"/>
+// <input type="email"/>
+// <input type="file"/>
+// <input type="hidden"/>
+// <input type="image"/>
+// <input type="month"/>
+// <input type="number"/>
+// <input type="password"/>
+// <input type="radio"/>
+// <input type="range"/>
+// <input type="reset"/>
+// <input type="search"/>
+// <input type="submit"/>
+// <input type="tel"/>
+// <input type="time"/>
+// <input type="url"/>
+// <input type="week"/>
